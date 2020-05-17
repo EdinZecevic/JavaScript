fd65e787bd8c83f20c054125fa435575267134b4
EdinZecevic <zeka23edin@hotmail.com>
2020-05-17 15:57:28 +0200

    repeating basic sintax of JavaScript

--- Overal JavaScript/app.js
+++ Overal JavaScript/app.js
@@ -1,3 +1,33 @@
-//document.write("hello world")
-//alert("hello world")
-//console.log("hello world")
+//document.write("hello world");
+//alert("hello world");
+//console.log("hello world");
+
+/*document.write("hello world");
+alert("hello world");
+console.log("hello world");*/
+
+document.write("hello world");
+
+/*let name = "Edin Zecevic 501";
+
+document.write(name);
+document.write(name);
+document.write(name);*/
+
+let name = "Edin";
+let surname = "Zecevic";
+let fullName = name + " " + surname;
+
+document.write(fullName);
+
+const greeting = 'Hey it is "who are you" me now';
+const greetings = "Hey it is 'who are you now";
+document.write(greeting);
+document.write(greetings);
+
+const number = 5;
+const numbers = 4.125;
+
+let total = number + numbers;
+
+document.write(total);
