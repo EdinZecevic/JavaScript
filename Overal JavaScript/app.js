93485db99c57898d0213352c6b362d850c363955
EdinZecevic <zeka23edin@hotmail.com>
2020-05-17 17:38:57 +0200

    repeating sintax of type of variables, arrays and implementing functions

--- Overal JavaScript/app.js
+++ Overal JavaScript/app.js
@@ -4,19 +4,19 @@
 
 /*document.write("hello world");
 alert("hello world");
-console.log("hello world");*/
+console.log("hello world");
 
 document.write("hello world");
 
-/*let name = "Edin Zecevic 501";
+let name = "Edin Zecevic 501";
 
 document.write(name);
 document.write(name);
-document.write(name);*/
+document.write(name);
 
 let name = "Edin";
 let surname = "Zecevic";
-let fullName = name + " " + surname;
+let fullName = name + " " + surname; //string concatination
 
 document.write(fullName);
 
@@ -25,9 +25,48 @@ const greetings = "Hey it is 'who are you now";
 document.write(greeting);
 document.write(greetings);
 
-const number = 5;
+let number = 5;
 const numbers = 4.125;
 
 let total = number + numbers;
 
 document.write(total);
+
+number += 45; // 50 (take the number and 45)
+number++;
+document.write(number);
+
+let number1 = "5";
+let number2 = "6";
+let result = number1 - number2;
+console.log(result);
+
+let number1 = 10;
+let number2 = 15;
+let result = number1 + number2;
+console.log(result);
+
+console.log(typeof result); //print the type of the variable
+
+const friends = ["Sinan", "Sakic", "Ibro", "Ramiz", 45, undefined];
+console.log(friends[0]);
+
+let bestFriend = friends[2];
+console.log(bestFriend);*/
+
+let name = "Sinan";
+
+function helloWorld(name) {
+  console.log("Ko je " + name);
+}
+
+function calculator(num1, num2) {
+  sum = num1 + num2;
+  div = num1 - num2;
+  tim = num1 * num2;
+  del = num1 / num2;
+
+  return sum;
+}
+const resault = calculator(4, 5);
+console.log(resault);
