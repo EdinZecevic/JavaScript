0fc2cba3899adfda5318fc2fd01aeeef8a7d2464
EdinZecevic <zeka23edin@hotmail.com>
2020-06-25 13:29:44 +0200

    addToStorage(), setBackToDefault() functions implementing

---
+++ DOMProjects/groceryBud/app.js
@@ -0,0 +1,79 @@
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+let editElement;
+let editFlag = false;
+let editID = "";
+
+form.addEventListener("submit", addItem);
+
+clearBtn.addEventListener("click", clearItems);
+
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+
+  if (value && !editFlag) {
+    const element = document.createElement("article");
+    element.classList.add("grocery-item");
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = ` <p class="title">${value}</p>
+            <div class="btn-container">
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>`;
+    list.appendChild(element);
+    displayAlert("Item added on list", "success");
+    container.classList.add("show-container");
+    addToLocalStorage(id, value);
+    setBackToDefault();
+  } else if (!value && editFlag) {
+    console.log("edditing");
+  } else {
+    displayAlert("plese enter value", "danger");
+  }
+}
+
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  setTimeout(function () {
+    alert.textContent = text;
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
+
+function addToLocalStorage(id, value) {
+  console.log("added to local storage");
+}
+function setBackToDefault() {
+  grocery.value = "";
+  editFlag = false;
+  editID = false;
+  submitBtn.textContent = "submit";
+}
+
+function clearItems() {
+  const items = document.querySelectorAll(".grocery-item");
+  if (items.length > 0) {
+    items.forEach(function (item) {
+      list.removeChild(item);
+    });
+  }
+  container.classList.remove("show-container");
+  displayAlert("List is Empty", "danger");
+  setBackToDefault();
+}
